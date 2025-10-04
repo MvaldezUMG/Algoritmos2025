13cc02f34b850dea7100ebd3b8bc4615f420bf99
Marco Tulio Valdez <mtulio1991@live.com>
2025-10-04 17:45:34 -0600

    editar estudiante

--- clase11/excel.py
+++ clase11/excel.py
@@ -32,24 +32,43 @@ def escribir_datos(nombre_archivo, carnet, nombre, carrera):
     #Guardamos en disco
     libro.save(nombre_archivo)
 
+def editar_datos(nombre_archivo, carnet, nombre, carrera):
+    libro = openpyxl.load_workbook(nombre_archivo)
+    nombre_primera_hoja = libro.sheetnames[0]
+    fila_encontrada = -1
+    hoja = libro[nombre_primera_hoja]
+    for fila in range(2, hoja.max_row + 1):
+        if hoja["A" + str(fila)].value == carnet:
+            fila_encontrada = fila
+            break
+    if fila_encontrada > -1:
+        hoja["B"+ str(fila_encontrada)]= nombre
+        hoja["C" + str(fila_encontrada)] = carrera
+        libro.save(nombre_archivo)
+
 while True:
+    nombre_archivo = ""
     print("1. Crear libro excel")
     print("2. Leer datos de libro excel")
     print("3. Agregar datos a libro excel")
+    print("4. Editar datos en libro excel")
     print("0. Salir")
 
     opcion = input("Ingrese la opcion: \n")
     if opcion == "1":
-        nombre = input("Ingrese el nombre del archivo\n")
+        nombre_archivo = input("Ingrese el nombre del archivo\n")
         crear_libro(nombre + ".xlsx")
     if opcion == "2":
-        nombre = input("Ingrese el nombre del archivo \n")
-        leer_datos(nombre + ".xlsx")
+        leer_datos(nombre_archivo + ".xlsx")
     if opcion == "3":
-        nombre_archivo = input("Ingrese el nombre del archivo \n")
         carnet = input("ingrese el carnet del estudiante\n")
         nombre = input("ingrese el nombre del estudiante\n")
         carrera = input("ingrese la carrera del estudiante \n")
         escribir_datos(nombre_archivo + ".xlsx", carnet, nombre, carrera)
+    if opcion == "4":
+        carnet = input("ingrese el carnet del estudiante\n")
+        nombre = input("ingrese el nombre del estudiante\n")
+        carrera = input("ingrese la carrera del estudiante \n")
+        editar_datos(nombre_archivo + ".xlsx", carnet, nombre, carrera)        
     if opcion == "0":
         break
